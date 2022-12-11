0391f9c0e22276ea7143893f6b74dfec1175de94
naTandpgithub <phutan.tx@gmail.com>
2022-12-11 12:15:39 +0700

    update1

--- src/control/main.js
+++ src/control/main.js
@@ -2,3 +2,50 @@ import Product from "../models/product.js";
 import ProductService from "../services/service.js";
 
 const productService = new ProductService();
+
+const getEle = (id) => document.getElementById(id);
+
+const renderHTML = (data) => {
+  if (data && data.length > 0) {
+    const result = data.reduce((content, item) => {
+      return (content += `
+                <div   class="d-flex m-3  ">
+                    <div>
+                    <img src="${item.img}" alt=""   class="img-fluid"   style="width: 300px"/>
+                    </div>
+                    <div>
+                        <p>${item.name}</p>
+                        <p>${item.backCamera}</p>
+                        <p>${item.frontCamera}</p>
+                        <p>${item.screen}</p>
+                        <p>${item.type}</p>
+                        <p>${item.price}</p>
+
+                    </div>
+                           
+                </div>
+            
+            
+            `);
+    }, "");
+    getEle("product").innerHTML = result;
+    return;
+  }
+  getEle("product").innerHTML = "";
+};
+
+const getListProduct = () => {
+  productService
+    .getListProductApi()
+    .then((rs) => {
+      if (rs.statusText === "OK") {
+        console.log(rs);
+        renderHTML(rs.data);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getListProduct();
--- src/services/service.js
+++ src/services/service.js
@@ -1,10 +1,9 @@
 class ProductService {
-    getListProductApi = ()=>{
-        
-    }
-
-
-
-
+  getListProductApi() {
+    return axios({
+      url: "https://6385f954875ca3273d4c01fb.mockapi.io/api/product",
+      method: "GET",
+    });
+  }
 }
 export default ProductService;
